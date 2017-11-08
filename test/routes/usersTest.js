592ebe5aca2bfc0f4c05f99f96be71556faaf9eb
pdvdev <pdvdev@yandex.ru>
2017-11-08 16:22:37 +0300

    create app

---
+++ test/routes/usersTest.js
@@ -0,0 +1,19 @@
+﻿'use strict';
+
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const chaiHttp = require('chai-http');
+
+const server = require('../../app');
+
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+chai.use(chaiAsPromised);
+
+describe('users', () => {
+  it('getUsers returns expected status', () => {
+    return expect(chai.request(server)
+      .get('/users')).to.eventually.have.status(200);
+  });
+});
